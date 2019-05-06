2b74e49891f8a6fb79ed20a561b6fb9c2a88dc09
Yousef Michael <yousef19@meet.mit.edu>
2019-05-06 04:51:37 +0300

    hi

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+
+from flask import Flask, flash, render_template, url_for, redirect, request
+from flask import session as login_session
+from flask_session import *
+import requests,json
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World test!'
+    return render_template('temre_bday.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
